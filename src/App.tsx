4f12d60ca48a366dfee590c189c8077621186467
LeeHoSeop <llss2ssll@gmail.com>
2019-08-14 17:43:41 +0900

    typescript_todo

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import TodoListContainer from './containers/TodoListContainer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TodoListContainer />
+      </div>
+    );
+  }
+}
+
+
+export default App;
